99779185de8e5dbe93b7ecca8b380d6853feb7d7
Sindre Sivertsen <sindrejohan1@gmail.com>
2025-01-19 15:01:12 +0100

    Improve nav bar

--- indexgenerator.sh
+++ indexgenerator.sh
@@ -21,24 +21,48 @@ done
 rm -f "www/_index.md"
 
 # Create article navigation page
-current_year="-"
+# Create article navigation page
+echo '<div class="posts">' > www/_index.md
+echo '  <h2>Recent Posts</h2>' >> www/_index.md
+echo '  <ul>' >> www/_index.md
+
 for name in $(find articles -type f -exec basename {} \; | sort -ur | sed 's/\.md//'); do
   # Check if the markdown file has 'published: true'
   if grep -q "published: true" "articles/$name.md"; then
     year=$(echo $name | sed -E 's/([0-9]{4})-([0-9]{2})-([0-9]{2})-(.*)/\1/')
     month=$(echo $name | sed -E 's/([0-9]{4})-([0-9]{2})-([0-9]{2})-(.*)/\2/')
     day=$(echo $name | sed -E 's/([0-9]{4})-([0-9]{2})-([0-9]{2})-(.*)/\3/')
-    title=$(echo $name | sed -E 's/([0-9]{4})-([0-9]{2})-([0-9]{2})-(.*)/\4/')
-    nice_title=$(echo "$year-$month-$day - $(echo ${title:0:1} | tr  '[a-z]' '[A-Z]' )${title:1}" | sed -e 's/-/ /g')
-    if [ $year != $current_year ]; then
-      echo -e "\n## $year\n" >> www/_index.md
-    fi
-    current_year=$year
-    echo "- [$nice_title](public/$name.html)" >> www/_index.md
+    title=$(echo $name | sed -E 's/([0-9]{4})-([0-9]{2})-([0-9]{2})-(.*)/\4/' | sed 's/-/ /g')
+    # Capitalize first letter of title
+    title=$(echo ${title:0:1} | tr '[a-z]' '[A-Z]')${title:1}
+    
+    # Convert month number to name
+    case $month in
+      "01") month_name="January" ;;
+      "02") month_name="February" ;;
+      "03") month_name="March" ;;
+      "04") month_name="April" ;;
+      "05") month_name="May" ;;
+      "06") month_name="June" ;;
+      "07") month_name="July" ;;
+      "08") month_name="August" ;;
+      "09") month_name="September" ;;
+      "10") month_name="October" ;;
+      "11") month_name="November" ;;
+      "12") month_name="December" ;;
+    esac
+    
+    echo "    <li>" >> www/_index.md
+    echo "      <span class=\"date\">$month_name $day, $year</span>" >> www/_index.md
+    echo "      <a href=\"/public/$name.html\">$title</a>" >> www/_index.md
+    echo "    </li>" >> www/_index.md
   else
     echo "Skipping $name (not published)"
   fi
 done
+
+echo "  </ul>" >> www/_index.md
+echo "</div>" >> www/_index.md
 
 pandoc \
   --standalone \

e81680e19d404ac57dd1926e5e78db22c283be1b
Sindre Sivertsen <sindrejohan1@gmail.com>
2025-01-19 11:17:35 +0100

    Move all web into www dir

--- indexgenerator.sh
+++ indexgenerator.sh
@@ -1,16 +1,21 @@
 #! /usr/bin/env bash
 
+root_dir="www/"
+articles_dir="www/articles"
+
+# Convert articles to html
 for article in $(ls articles); do
 	echo $article
 	pandoc \
 	--standalone \
 	--template template.html \
 	"articles/$article" \
-	-o "public/$(basename $article .md).html"
+	-o "www/public/$(basename $article .md).html"
 done
 
-rm -f _index.md
+rm -f "www/_index.md"
 
+# Create article navigation page
 current_year="-"
 for name in $(find articles -type f -exec basename {} \; | sort -ur | sed 's/\.md//'); do
   year=$(echo $name | sed -E 's/([0-9]{4})-([0-9]{2})-([0-9]{2})-(.*)/\1/')
@@ -19,7 +24,7 @@ for name in $(find articles -type f -exec basename {} \; | sort -ur | sed 's/\.m
   title=$(echo $name | sed -E 's/([0-9]{4})-([0-9]{2})-([0-9]{2})-(.*)/\4/')
   nice_title=$(echo "$year-$month-$day - $(echo ${title:0:1} | tr  '[a-z]' '[A-Z]' )${title:1}" | sed -e 's/-/ /g')
   if [ $year != $current_year ]; then
-    echo -e "\n## $year\n" >> _index.md
+    echo -e "\n## $year\n" >> www/_index.md
   fi
   current_year=$year
   echo "- [$nice_title](public/$name.html)" >> _index.md
@@ -30,4 +35,4 @@ pandoc \
 	--standalone \
 	--template index_template.html \
 	_index.md \
-	-o index.html
+	-o www/index.html
